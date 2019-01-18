e6dc4dc96b92f088b5cf5c385ba78ad302c1ddea
Ian Aranha <ianorama@gmail.com>
2019-01-17 16:14:56 -0800

    Updated blog app

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import JSONPlaceholder from '../apis/JSONPlaceholder'
+
+export const fetchPosts = () => async dispatch => {
+        const response = await JSONPlaceholder.get('/posts');
+
+        dispatch({ 
+            type: 'FETCH_POSTS',
+            payload: response.data
+        })
+    }
+
+    
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+}
+
+const _fetchUser = _.memoize( async(id, dispatch) => {
+    const response = await JSONPlaceholder.get(`/users/${id}`)
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+})
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+    return  <div className='ui container'>
+                <div>
+                    <PostList />
+                </div>
+            </div>
+}
+
+export default App;
